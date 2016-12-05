e1c42358a67b24c592d3c6c984bbe2a08bf3ffe8
HCI01\meads <meads@hcpci.com>
2016-12-05 14:08:52 -0500

    added user info

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://meads@hcpci.com:usfbulls23@ds050869.mlab.com:50869/christmas_list');
+mongoose.connect('mongodb://meads:meads@ds050869.mlab.com:50869/christmas_list');
