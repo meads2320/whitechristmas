ffb728d29571975b3c30654fbd00ded724582479
HCI01\meads <meads@hcpci.com>
2016-12-06 08:54:47 -0500

    updated env variables for db

--- model/db.js
+++ model/db.js
@@ -1,2 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://meads:meads@ds050869.mlab.com:50869/christmas_list');
+var user = process.env.MONGO_USER;
+var pass = process.env.MONGO_PASS;
+var url = (user && pass) ? 'mongodb://' + user + ':' + pass + '@ds050869.mlab.com:50869/christmas_list' : "mongodb://localhost:27017/christmas_list";
+mongoose.connect(url);
